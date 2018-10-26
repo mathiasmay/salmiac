481155cd3c45dcd2e18223b90f7e4bc43bb688de
mathiasmay <44480401+mathiasmay@users.noreply.github.com>
2018-10-26 05:18:53 +0200

    Stile adapted

--- Math/FunctionValues.java
+++ Math/FunctionValues.java
@@ -28,10 +28,12 @@ import salmiac.Math.*;
 
 import java.math.BigInteger;
 
-public class FunctionValues {
+public class FunctionValues
+{
     
     //--------------------------------------------------
-    public static int Pow_positive_integer_power(int b,int p) {
+    public static int Pow_positive_integer_power(int b,int p)
+    {
         int toreturn;
         
         if (p==0)
@@ -52,10 +54,14 @@ public class FunctionValues {
         
         return toreturn;
     }
-    public static BigInteger Pow_positive_integer_power(BigInteger b,int p) {
+    
+    public static BigInteger Pow_positive_integer_power(BigInteger b,int p)
+    {
         return b.pow(p);
     }
-    public static float Pow_positive_integer_power(float b,int p) {
+    
+    public static float Pow_positive_integer_power(float b,int p)
+    {
         float toreturn;
         
         if (p==0)
@@ -69,7 +75,9 @@ public class FunctionValues {
         
         return (float)toreturn;
     }
-    public static double Pow_positive_integer_power(double b,int p) {
+    
+    public static double Pow_positive_integer_power(double b,int p)
+    {
         double toreturn;
         
         if (p==0)
@@ -85,53 +93,73 @@ public class FunctionValues {
     }
 
     //--------------------------------------------------
-    public static double Sin(double x) {
+    public static double Sin(double x)
+    {
         return Math.sin(x);
     }
-    public static float Sin(float x) {
+    
+    public static float Sin(float x)
+    {
         return (float)Math.sin(x);
     }
 
     //--------------------------------------------------
-    public static double Cos(double x) {
+    public static double Cos(double x)
+    {
         return Math.cos(x);
     }
-    public static float Cos(float x) {
+    
+    public static float Cos(float x)
+    {
         return (float)Math.cos(x);
     }
 
     //--------------------------------------------------
-    public static float Exp(float x) {
+    public static float Exp(float x)
+    {
         return (float)Math.exp(x);
     }
-    public static double Exp(double x) {
+    
+    public static double Exp(double x)
+    {
         return Math.exp(x);
     }
 
     //--------------------------------------------------
-    public static float Sqrt(float x) {
+    public static float Sqrt(float x)
+    {
         return (float)Math.sqrt(x);
     }
-    public static double Sqrt(double x) {
+    
+    public static double Sqrt(double x)
+    {
         return Math.sqrt(x);
     }
 
     //Abs(x)-------------------------------------------------
-    public static float Abs(float x) {
+    public static float Abs(float x)
+    {
         return (float)(Math.abs(x));
     }
-    public static double Abs(double x) {
+    
+    public static double Abs(double x)
+    {
         return Math.abs(x);
     }
 
     //Round(x,a)-------------------------------------------------
-    public static float Round(float x, float a) {
+    public static float Round(float x, float a)
+    {
         return (float)(Math.round(x/a)*a);
     }
-    public static double Round(double x, double a) {
+    
+    public static double Round(double x, double a)
+    {
         return Math.round(x/a)*a;
     }
-    public static long Round(long x, long a) {
+    
+    public static long Round(long x, long a)
+    {
         return
         (long)
         (
@@ -147,13 +175,18 @@ public class FunctionValues {
     }
 
     //Floor(x,a)-------------------------------------------------
-    public static float Floor(float x, float a) {
+    public static float Floor(float x, float a)
+    {
         return (float)(Math.floor(x/a)*a);
     }
-    public static double Floor(double x, double a) {
+    
+    public static double Floor(double x, double a)
+    {
         return Math.floor(x/a)*a;
     }
-    public static long Floor(long x, long a) {
+    
+    public static long Floor(long x, long a)
+    {
         return
         (long)
         (
@@ -169,106 +202,125 @@ public class FunctionValues {
     }
 
     //WholeNumber_which_is_the_next_smaller_or_equal_whole_number(x)-------------------------------------------------
-    public static int WholeNumber_which_is_the_next_smaller_or_equal_whole_number(float x) {
+    public static int WholeNumber_which_is_the_next_smaller_or_equal_whole_number(float x)
+    {
         return (int)Math.floor(x);
     }
-    public static int WholeNumber_which_is_the_next_smaller_or_equal_whole_number(double x) {
+    
+    public static int WholeNumber_which_is_the_next_smaller_or_equal_whole_number(double x)
+    {
         return (int)Math.floor(x);
     }
 
     //WholeNumber_which_has_the_next_smaller_or_equal_magnitude(x)-------------------------------------------------
-    public static int WholeNumber_which_has_the_next_smaller_or_equal_magnitude(float x) {
+    public static int WholeNumber_which_has_the_next_smaller_or_equal_magnitude(float x)
+    {
         return (int)(Math.signum(x)*Math.floor(Math.abs(x)));
     }
-    public static int WholeNumber_which_has_the_next_smaller_or_equal_magnitude(double x) {
+    
+    public static int WholeNumber_which_has_the_next_smaller_or_equal_magnitude(double x)
+    {
         return (int)(Math.signum(x)*Math.floor(Math.abs(x)));
     }
 
     
     
     //Max(list)-------------------------------------------------
-    public static float Max(float list[]) {
+    public static float Max(float list[])
+    {
         int n;
         float toreturn=list[0];
-        for (n=1; n<=list.length-1; n++) {
+        for (n=1; n<=list.length-1; n++)
+        {
             if (toreturn<list[n]) {toreturn=list[n];}
         }
         return toreturn;
     }
-    public static double Max(double list[]) {
+    
+    public static double Max(double list[])
+    {
         int n;
         double toreturn=list[0];
-        for (n=1; n<=list.length-1; n++) {
+        for (n=1; n<=list.length-1; n++)
+        {
             if (toreturn<list[n]) {toreturn=list[n];}
         }
         return toreturn;
     }
 
-    public static double Max(double a, double b) {
-        
+    public static double Max(double a, double b)
+    {
         if (a<b) {return b;}
         else     {return a;}
-        
     }
 
-    public static int Max(int a, int b) {
-        
+    public static int Max(int a, int b)
+    {
         if (a<b) {return b;}
         else     {return a;}
-        
     }
 
     
     
     //Min(list)-------------------------------------------------
-    public static float Min(float list[]) {
+    public static float Min(float list[])
+    {
         int n;
         float toreturn=list[0];
-        for (n=1; n<=list.length-1; n++) {
+        for (n=1; n<=list.length-1; n++)
+        {
             if (list[n]<toreturn) {toreturn=list[n];}
         }
         return toreturn;
     }
-    public static double Min(double list[]) {
+    
+    public static double Min(double list[])
+    {
         int n;
         double toreturn=list[0];
-        for (n=1; n<=list.length-1; n++) {
+        for (n=1; n<=list.length-1; n++)
+        {
             if (list[n]<toreturn) {toreturn=list[n];}
         }
         return toreturn;
     }
 
-    public static double Min(double a, double b) {
-        
+    public static double Min(double a, double b)
+    {
         if (b<a) {return b;}
         else     {return a;}
-        
     }
 
-    public static int Min(int a, int b) {
-        
+    public static int Min(int a, int b)
+    {
         if (b<a) {return b;}
         else     {return a;}
-        
     }
 
     //Mod(x,a)-------------------------------------------------
-    public static float Mod(float x, float a) {
-        return (float)(
+    public static float Mod(float x, float a)
+    {
+        return (float)
+        (
               x
             - (
-                (float)(
+                (float)
+                (
                     (long)(x/a)
                 )
               )
             * a
         );
     }
-    public static double Mod(double x, double a) {
-        return (double)(
+    
+    public static double Mod(double x, double a)
+    {
+        return (double)
+        (
               x
             - (
-                (double)(
+                (double)
+                (
                     (long)(x/a)
                 )
               )
@@ -312,12 +364,15 @@ public class FunctionValues {
     
 
     //--------------------------------------------------
-    public static double one_dimensional_linear_function_depending_on_two_points(double x, double x1, double y1, double x2, double y2) {
+    public static double one_dimensional_linear_function_depending_on_two_points(double x, double x1, double y1, double x2, double y2)
+    {
         double a0 = (x1*y2-x2*y1)/(x1-x2);
         double a1 = (y1-y2)/(x1-x2);
         return a0+a1*x;
     }
-    public static float one_dimensional_linear_function_depending_on_two_points(float x, float x1, float y1, float x2, float y2) {
+    
+    public static float one_dimensional_linear_function_depending_on_two_points(float x, float x1, float y1, float x2, float y2)
+    {
         float a0 = (x1*y2-x2*y1)/(x1-x2);
         float a1 = (y1-y2)/(x1-x2);
         return a0+a1*x;
